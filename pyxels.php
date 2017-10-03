c633f6fc9317aafc9a544791dfceab4f07e07865
chlorek <chlorek.cl@gmail.com>
2017-10-03 21:07:29 +0200

    added license notice

--- pyxels.php
+++ pyxels.php
@@ -1,3 +1,23 @@
+<!--
+pyxels - simple screen capture tool for Linux
+Copyright (C) 2017 Chlorek
+
+This file is part of pyxels.
+
+Pyxels is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Pyxels is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pyxels.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <?php
     //error_reporting(E_ALL); ini_set('display_errors', 1);
     if(!isset($_FILES['file'])) {
