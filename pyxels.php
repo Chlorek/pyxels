f068e5b452db11509fe802adf1dbe9edf5e0db31
chlorek <chlorek.cl@gmail.com>
2018-03-11 00:07:18 +0100

    added web screenshot editor

--- pyxels.php
+++ pyxels.php
@@ -21,7 +21,172 @@
 
     //error_reporting(E_ALL); ini_set('display_errors', 1);
     if(!isset($_FILES['file'])) {
-        echo 'No files in received request.';
+        if(isset($_GET['edit'])) {
+            ?>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Editor &bull; Pyxels</title>
+                    <style>
+                        body {
+                            background-color: #222;
+                            color: #eee;
+                            font-family: monospace;
+                        }
+
+                        #content {
+                            margin: 10px auto 0px auto;
+                            width: 600px;
+                            text-align: center;
+                        }
+
+                        #controls {
+                            width: 600px;
+                            text-align: center;
+                            margin-top: 10px;
+                            border-top: 2px solid #888;
+                            border-bottom: 2px solid #888;
+                            font-weight: bold;
+                            font-size: 18pt;
+                            height: 36px;
+                            line-height: 36px;
+                            -webkit-touch-callout: none; /* iOS Safari */
+                            -webkit-user-select: none; /* Safari */
+                             -khtml-user-select: none; /* Konqueror HTML */
+                               -moz-user-select: none; /* Firefox */
+                                -ms-user-select: none; /* Internet Explorer/Edge */
+                                    user-select: none;
+                        }
+
+                        #controls > .highlight:hover {
+                            text-shadow: red 2px 2px;
+                        }
+
+                        #brushSize {
+                            width: 25px;
+                            display: inline-block;
+                        }
+
+                        .separator {
+                            height: 26px;
+                            width: 2px;
+                            background-color: #888;
+                            display: inline-block;
+                            position: relative;
+                            top: 4px;
+                        }
+
+                        #colorPicker {
+                            position: relative;
+                            top: -3px;
+                        }
+                    </style>
+                    <script>
+                        var color = '#ff0000'
+
+                        var mouseDown = false;
+                        function draw(event) {
+                            if(mouseDown) {
+                                var x = event.x - canvas.offsetLeft;
+                                var y = event.y - canvas.offsetTop;
+
+                                ctx.beginPath();
+                                ctx.arc(x, y, brushSize, 0, 2*Math.PI, false);
+                                ctx.fillStyle = color;
+                                ctx.fill();
+                            }
+                        }
+
+                        function startDraw(event) {
+                            mouseDown = true;
+                            draw(event);
+                        }
+
+                        function stopDraw(event) {
+                            mouseDown = false;
+                        }
+
+                        function updateColor(event) {
+                             color = event.target.value;
+                        }
+
+                        function init(url){
+
+                        }
+
+                        window.onload = function() {
+                            var img = new Image();
+                            img.onload = function() {
+                                canvas = document.getElementById('editorCanvas');
+                                canvas.width = this.width;
+                                canvas.height = this.height;
+                                ctx = canvas.getContext("2d");
+                                ctx.drawImage(img, 0, 0);
+                                canvas.addEventListener("mousedown", startDraw, false);
+                                canvas.addEventListener("mouseup", stopDraw, false);
+                                canvas.addEventListener("mousemove", draw, false);
+                                var colorPicker = document.getElementById('colorPicker');
+                                colorPicker.addEventListener("input", updateColor, false);
+                                colorPicker.addEventListener("change", updateColor, false);
+                                color = colorPicker.value;
+                            };
+                            img.src = "<?php echo 'http://' . $_SERVER['SERVER_NAME'] . '/uploads/' . $_GET['edit'] ?>";
+                        };
+
+                        var brushSize = 10;
+
+                        function biggerBrush() {
+                            brushSize += 2;
+                            if(brushSize > 30)
+                                brushSize = 30;
+                            document.getElementById('brushSize').innerHTML = brushSize;
+                        }
+
+                        function smallerBrush() {
+                            brushSize -= 2;
+                            if(brushSize <= 0)
+                                brushSize = 2;
+                            document.getElementById('brushSize').innerHTML = brushSize;
+                        }
+
+                        function save() {
+                            canvas.toBlob(function(blob) {
+                                var data = new FormData();
+                                data.append("file", blob, '.png');
+                                var request = new XMLHttpRequest();
+                                request.open('POST', '<?php echo $_SERVER['DOCUMENT_URI'] ?>', true);
+                                request.onload = function() {
+                                    if(request.status === 200) {
+                                        window.location = request.responseText;
+                                    } else
+                                        alert('An error occurred ('+request.status+')');
+                                };
+                                request.send(data);
+                            });
+                        }
+                    </script>
+                </head>
+                <body>
+                    <div id="content">
+                        <canvas width="0" height="0" id="editorCanvas">
+                            HTML5 canvas not supported by your browser.
+                        </canvas>
+                        <div id="controls">
+                            <span class="highlight" onclick="biggerBrush()">+</span>
+                            <span id="brushSize">10</span>
+                            <span class="highlight" onclick="smallerBrush()">-</span>
+                            <span class="separator"></span>
+                            <input id="colorPicker" type="color" value="#ff0000">
+                            <span class="highlight" onclick="save()">Save</span>
+                        </div>
+                    </div>
+
+                </body>
+            </html>
+            <?php
+        }
+        else
+            echo 'Empty request.';
     }
     else {
         /* You can keep your screenshots named as in request, but this way you
@@ -33,7 +198,12 @@
 
         $tmpName = $_FILES['file']['tmp_name'];
         move_uploaded_file($tmpName, $output);
-        echo 'http://' . $_SERVER['SERVER_NAME'] . '/' . $output;
         //echo $_FILES['file']['error'];
+
+        // Output direct URL to image or to edit-page
+        if(isset($_GET['edit']))
+            echo 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER["DOCUMENT_URI"] . '?edit=' . $_GET['img'];
+        else
+            echo 'http://' . $_SERVER['SERVER_NAME'] . '/' . $output;
     }
 ?>
